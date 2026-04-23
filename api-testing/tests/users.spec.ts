4bb3beabb2fdeca9adca51f1b9bf5e6ccf688c9f
chelomagic <67094541+chelomagic@users.noreply.github.com>
2026-04-23 17:24:16 -0300

    feat: add POST test for creating a new post

--- api-testing/tests/users.spec.ts
+++ api-testing/tests/users.spec.ts
@@ -22,3 +22,20 @@ test ('Get user returns a list of 10 users', async ({request}) => {
   expect(body.length).toBe(10);
   
 } )
+
+
+test('Post creates a new post', async ({ request }) => {
+  const response = await request.post('https://jsonplaceholder.typicode.com/posts', {
+    data: {
+      title: 'my first post',
+      content: 'hello world',
+      userId: 1
+    }
+  });
+
+  
+
+  const body = await response.json();
+  expect(body.title).toBe('my first post');
+  expect(body.id).toBeDefined();
+});
